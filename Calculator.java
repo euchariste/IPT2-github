1a7961af61165aa9ad9671858ed76e5a7fe0eefa
Espiritu_Osio_Pamintuan <dionelanijayr@gmail.com>
2024-09-12 14:55:00 +0800

    Add multiplication method

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,14 @@
 public class Calculator {
 public static int add(int a, int b) {
-return a + b; }
+return a+b;
+}
+
 public static int subtract(int a, int b) {
-return a - b; }
-public static void main(String[] args) {
+return a-b; }
+public static int multiply(int a, int b) {
+return a*b; }
+public static void main(String[] args){
 System.out.println(Simple Calculator: );
 System.out.println(Addition:  + add(5,3));
-System.out.println(Subtraction:  + subtract (5,3));}}
+System.out.println(Subtraction:  + subtract(5,3));
+System.out.println(Multiplication:  + multiply(5,3)); }}
