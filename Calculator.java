2b877e4a11350f932b3dd6ab058fc07bf73ff9c4
Espiritu_Osio_Pamintuan <dionelanijayr@gmail.com>
2024-09-12 14:20:22 +0800

    Add addition method

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,6 @@
-public class Calculator { public static void main(String[]args){ System.out.println(Simple Calculator: ); }}
+public class Calculator {
+public static int add( int a, int b){
+return a + b; }
+public static void main(String[]args){
+System.out.println(Simple calculator: );
+System.out.println(Addition:  + add(5, 3)); }}
